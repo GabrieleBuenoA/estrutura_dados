cb25f411e06a2c9e63f24dff0fcabc063c45385c
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2026-08-04 22:19:47 +0000

    aula 08/04 - vetor

---
+++ atv_array/ex3.java
@@ -0,0 +1,20 @@
+package atv_array;
+public class ex3{
+    public static void main() {
+        VetorEstatico vetor = new VetorEstatico(10);
+
+        vetor.inserir("A");
+        vetor.inserir("B");
+        vetor.inserir("C");
+        vetor.inserir("D");
+        vetor.inserir("E");
+        vetor.inserir("F");
+
+        vetor.imprimir();
+
+        vetor.remover(2);
+        vetor.remover(5);
+
+        vetor.imprimir();
+    }
+}
