936a65a0847449ff803ac07d2cc94b91c3d84b93
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2026-08-06 22:39:51 +0000

    aula 06/08

--- atv_AgendaTelefonica/Agenda.java
+++ atv_AgendaTelefonica/Agenda.java
@@ -11,7 +11,7 @@ public Agenda(int tamanho) {
     this.agenda = new Contato[tamanho];
 }
 
-public boolean verificarNome(String nome) {
+public boolean verificarNomeLivre(String nome) {
     for (Contato contato : agenda) {
         if (nome.equals(contato.getNome())) {
             System.out.printf("Contato já registrado");
@@ -22,7 +22,7 @@ public boolean verificarNome(String nome) {
     }
 }
 
-public boolean verificarTelefone(String telefone) {
+public boolean verificarTelefoneLivre(String telefone) {
     for (Contato contato : agenda) {
         if (telefone.equals(contato.getTelefone())){
             System.out.printf("Número já registrado");
@@ -33,14 +33,82 @@ public boolean verificarTelefone(String telefone) {
     }
 }
 
+public int getPosição(Contato contato){
+    if (!verificarNomeLivre(contato.getNome()) || !verificarTelefoneLivre(contato.getTelefone())) {
+        for (int i = 0; i < TAMANHO; i++) {
+            if (agenda[i] != null && agenda[i].getNome().equals(contato.getNome()) || agenda[i].getTelefone().equals(contato.getTelefone())) {
+            return i;
+            }
+        }
+    }
+    return -1;
+}
+
 public void adicionarContato(Contato contato) {
-    for (int i = 0; i < TAMANHO; i++) {
-        if (agenda[i] == null) {
-            agenda[i] = contato;
+    if (!verificarNomeLivre(contato.getNome())){ 
+        System.out.println("Nome de contato já registrado. Não é possível adicionar.");
+        return;
+    } 
+    else if (!verificarTelefoneLivre(contato.getTelefone())){
+        System.out.println("Número já associado. Não é possível adicionar.");
+    } 
+    else if (verificarNomeLivre((contato.getNome())) && verificarTelefoneLivre(contato.getTelefone())) {
+        for (int i = 0; i < TAMANHO; i++) {
+            if (agenda[i] == null) {
+                agenda[i] = contato;
+                System.out.println("Contato adicionado com sucesso.");
+                return;
+            }
+        }
+    }
+    else{
+        System.out.println("Agenda cheia. Não é possível adicionar mais contatos.");
+    }
+    
+}
+
+public void removerContato(Contato nomeContato) {
+    if (!verificarNomeLivre(nomeContato.getNome())) {
+        for (int i = 0; i < TAMANHO; i++) {
+            if (agenda[i] != null && agenda[i].getNome().equals(nomeContato.getNome())) {
+                agenda[i] = null;
+                System.out.println("Contato removido com sucesso.");
+                return;
+            }
+        } 
+    } else {
+        System.out.println("Contato não encontrado.");
+    }
+}
+
+public void atualizarContato(Contato contato, String novoNome, String novoTelefone, String novoEmail) {
+    if (!verificarNomeLivre(contato.getNome()) || !verificarTelefoneLivre(contato.getTelefone())) {
+        var indice = getPosição(contato);
+        
+        
+
+        for agenda[indice]{
+
+            contato.setNome(novoNome); 
+            contato.setTelefone(novoTelefone);
+            contato.setEmail(novoEmail);
+
+            (novoNome, novoTelefone, novoEmail);
+
+
+            System.out.println("Contato atualizado com sucesso.");
+            return;
+        }
+
+
+
+        if (agenda[i] != null && agenda[i].getNome().equals(contato.getNome())) {
+            agenda[i] = new Contato(novoNome, novoTelefone, novoEmail);
+            System.out.println("Contato atualizado com sucesso.");
             return;
         }
     }
-    System.out.println("Agenda cheia. Não é possível adicionar mais contatos.");
+    System.out.println("Contato não encontrado.");
 }
 
 
--- atv_AgendaTelefonica/Contato.java
+++ atv_AgendaTelefonica/Contato.java
@@ -16,12 +16,32 @@ public class Contato {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getTelefone() {
         return telefone;
     }
 
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
     public String getEmail() {
         return email;
+    }   
+
+    public void setEmail(String email) {
+        this.email = email;
     }
+
+    public void exibirContato() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Email: " + email);
+    }
+
+
 
 }
