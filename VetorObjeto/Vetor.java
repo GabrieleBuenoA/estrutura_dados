0b43db3441cc464140b24fe20b1fabb095fda8c5
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2026-08-13 22:30:51 +0000

    meh

--- VetorObjeto/Vetor.java
+++ VetorObjeto/Vetor.java
@@ -9,7 +9,47 @@ public class Vetor<T> {
         elementos = (T[]) new Object[quantidade];
         tamanho = 0;
         
+    }
 
+    public void inserir(String elemento) {
+        if (tamanho == elemento.length()) {
+            expandir ();
+        } else {
+        elementos[tamanho] = elemento;
+        tamanho++;
+        }
+    }
+
+    private void expandir() {
+        String[] novo = new String[elementos.length * 2];
+        for (int i = 0; i < elementos.length; i++) {
+            novo[i] = elementos[i];
+        }
+        this.elementos = novo;
+    }
+      
+    private void reduzir() {
+        
+        if (tamanho <= elementos.length/4) { ///quando diminuir
+            T[] novo = (T[]) new String[elementos.length/2]; //quanto diminuir
+            for (int i = 0; i < elementos.length; i++) {
+                novo[i] = elementos[i];
+        }
+        elementos = novo;
+        }   
+    }
+
+    public void remover (int indice) {
+        if (indice < 0 || indice >= tamanho){
+            System.out.println("Indice inválido");
+            return;
+        }
+        for (int i = indice; i < tamanho; i++){
+            elementos[i] = elementos[i+1];
+        }
+
+        elementos[tamanho-1] = null;
+        tamanho--;
+        reduzir();
     }
     
-}
